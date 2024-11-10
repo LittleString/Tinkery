690f3519eb06e7c8a17602c9366350292cb429e7
LittleString <littlestring899@gmail.com>
2024-11-09 22:25:57 -0600

    Main formatting, data structure for items

---
+++ Tinkery.js
@@ -0,0 +1,26 @@
+import { items } from './items.js';
+
+// Put all icons in iconContainer
+function initIconPanel() {
+    for (let i = 0; i < items.length; i++) {
+        // Create icon element
+        let iconDiv = document.createElement('div');
+        iconDiv.classList.add('icon');
+        iconDiv.setAttribute('title', items[i].tooltip);
+
+        // Create icon image
+        let image = document.createElement('img');
+        image.src = items[i].iconLocation;
+        iconDiv.appendChild(image);
+
+        // Create icon label (item name text)
+        let iconName = document.createElement('p');
+        iconName.innerText = items[i].name;
+        iconDiv.appendChild(iconName);
+
+        // Append icon to iconContainer
+        document.getElementById("iconContainer").appendChild(iconDiv);
+    }
+};
+
+initIconPanel();
---
+++ items.js
@@ -0,0 +1,22 @@
+// JSON object containing all information about every item in Tinkerer's Workshop recipes
+
+let items = [
+    {
+        "name": "Tackle Box",
+        "tooltip": "Decreases chance of bait consumption",
+        "value": {
+            "copper": 0,
+            "silver": 0,
+            "gold": 1,
+            "platinum": 0
+        },
+        "iconLocation": "images/icons/Tackle_Box.png",
+        "wikiLink": "https://terraria.fandom.com/wiki/Tackle_Box",
+        "hardmode": false,
+        "nextCraftOption": "",
+        "priorCraftOption": ""
+    }
+]
+
+// Export all items for use throughout the app
+export { items }
